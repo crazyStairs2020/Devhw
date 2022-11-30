d9568c2beb707d42ee105a912b2d2f85840b8972
Paul Krupski <pjkrupski@gmail.com>
2022-11-30 18:05:22 -0500

    cart works with books but nothings sortable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import { useState } from "react";
+import bakeryData from "./assets/bakery-data.json";
 import bookData from "./assets/book-data.json";
 
-
-bookData.forEach((item) => {
+/* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
+bakeryData.forEach((item) => {
   item.image = process.env.PUBLIC_URL + "/" + item.image;
 });
+/* ############################################################## */
 
+function BakeryItem(props){
+  const item = props.item
+  const add = () =>
+  {props.setCart([...props.cart, item]) 
+  props.setTotal(props.total+item.price)}
+  return(
+    <div>
+     <img src={item.image} width="200" height="200"/>
+    <button onClick={add}>Select</button>
+    <div>
+      {item.name}
+    </div>
+    <div>
+     {item.price}
+    </div>
+    <div>
+     {item.description}
+    </div>
+    </div>
+
+  );
+}
 
 function Book(props) {
   const item = props.item
@@ -33,93 +56,27 @@ function Book(props) {
   );
 }
 
-function BookList(props) {
-  const books = []
-  return (
-    <div>
-      {books}
-    </div>
-
-  );
-}
-
 
 function App() {
+  // TODO: use useState to create a state variable to hold the state of the cart
+  /* add your cart state code here */
   const [cart, setCart] = useState([])
   const [total, setTotal] = useState(0)
-  const [display, setDisplay] = useState([bookData.map((item, index) => (
-    <Book item={item} cart={cart} setCart={setCart} total={total} setTotal={setTotal} />
-  )).map((number) =>
-  <li key={number.props.item.title.toString()}>
-   {number}
-   </li>
-   )])
-  const setBooks = () => bookData.map((item, index) => (
-    <Book item={item} cart={cart} setCart={setCart} total={total} setTotal={setTotal} />
-  ))
-
-  //const filtered = fruitsArray.filter(objects => { return objects.nutritions.sugar > 15});
-  const filter_fantasy = () => {  
-    setDisplay([display.at(0).filter(object => object.props.children.props.item.genre == "Fantasy")])
-  }
-  const filter_scifi = () => {
-    setDisplay([display.at(0).filter(object => object.props.children.props.item.genre == "Sci-fi")])
-  }
-  const filter_nonfiction = () => {
-    setDisplay([display.at(0).filter(object => object.props.children.props.item.genre == "non-fiction")])
-    console.log(display)
-  }
-  const filter_classic = () => {
-    setDisplay([display.at(0).filter(object => object.props.children.props.item.genre == "Classic")])
-    console.log("after classic ", display)
-  }
-  const filter_reset = () => {
-    setDisplay([bookData.map((item, index) => (
-      <Book item={item} cart={cart} setCart={setCart} total={total} setTotal={setTotal} />
-    )).map((number) =>
-    <li key={number.props.item.title.toString()}>
-     {number}
-     </li>
-     )])
-    console.log("after resetting ", display)
-  }
-  const sort_price = () => {
-    const lst = []
-    const temp = display.at(0).sort((a, b) => (a.props.children.props.item.price >= b.props.children.props.item.price) ? 1 : -1)
-    //setDisplay on lst.sort wont work without []
-    for(let i = 0; i < temp.length; i++){
-      lst.push(temp.at(i).props.children);
-    }
-    console.log(lst)
-    setDisplay([lst.map((number) =>
-      <li key={number.props.item.title.toString()}>
-       {number}
-       </li>
-       )])
-  }
   return (
     <div className="App">
-      <header className="App-header">
-        {display}
-        <p>
-          Edit <code>src/App.js</code> and save to r
-        </p>
-        <a
-          className="App-lin"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <button onClick={filter_fantasy}>Fantasy</button>
-      <button onClick={filter_scifi}>Sci-fi</button>
-      <button onClick={filter_nonfiction}>non-fiction</button>
-      <button onClick={filter_classic}>Classic</button>
-      <button onClick={filter_reset}>Reset</button>
-      <button onClick={sort_price}>Sort</button>
+      <h1>Mikes Pastries</h1> {/* TODO: personalize your bakery (if you want) */}
+
+      {bookData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+        <Book item={item} cart={cart} setCart={setCart} total={total} setTotal={setTotal}/>// replace with BakeryItem component
+      ))}
+      
+        <h2>Cart</h2>
+        {cart.map((item) => (
+        <div>{item.title}</div>
+      ))
+      }
       <div>{total}</div>
+      
     </div>
   );
 }
